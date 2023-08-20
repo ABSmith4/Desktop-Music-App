6ecf63b1f60ab9bc62cc0d4f0518cd93dac5124c
Ahmad Smith <Smith.Ahmad3@gmail.com>
2023-08-20 01:06:20 -0400

    added songcard functionality

---
+++ react-app/src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '8a7ba9a7ebmsh3d82ff0e32e10b5p15fb7bjsnc237c013e1e2');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
